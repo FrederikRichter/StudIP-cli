49f7a0aa35f386cac20b81f22eba6dfa4c9b6f34
FrederikRichter <Frederik.Richter@mailbox.org>
2024-01-20 12:33:33 +0100

    perfect argparser

--- app/main.py
+++ app/main.py
@@ -4,40 +4,51 @@
 
 import sys
 import argparse
-import tempfile
 
 # user imports
 from lib import *
 
 def cmdline_args():
     # Make parser object
+
+    default = []
+
     p = argparse.ArgumentParser(description=__doc__,
                                 formatter_class=argparse.RawDescriptionHelpFormatter, prog="studip-cli")
 
     p.add_argument("--session", action="store_true",
-                   help="creates a user session for great performance improvements") 
+                   help="creates a user session for great performance improvements")
 
     group = p.add_mutually_exclusive_group(required=True)
-    group.add_argument("list-courses", action="store_true",
+    group.add_argument("--list-courses", action="store_true",
                        help="list your courses")
-    group.add_argument("view-course", type=str,
+    group.add_argument("--view-course", type=str,
                        help="retrieves course info (list)",
-                       nargs="+",
-                       default=sys.stdin)
-    group.add_agrument("list-files", type=str,
+                       nargs="*",
+                       default=default)
+    group.add_argument("--list-files", type=str,
                        help="lists files in a list of folders (list)",
-                       nargs="+",
-                       default=sys.stdin)
-    group.add_argument("download", type=str,
+                       nargs="*",
+                       default=default)
+    group.add_argument("--download", type=str,
                        help="downloads files (list)",
-                       nargs="+",
-                       default=sys.stdin)
+                       nargs="*",
+                       default=default)
     return(p.parse_args())
 
 def main():
     """ Main program """
-    # Code goes over here.
+    args = cmdline_args()
 
+    # Check if any specified argument is not used
+    if not any(vars(args).values()):
+        data = sys.stdin.read().replace("\n", " ").rstrip().split(" ")
+        print("stdin", data)
+    else:
+        # Process the arguments after any specified option
+        for arg_name, arg_value in vars(args).items():
+            if arg_value:
+                print(f"{arg_name} args:", arg_value)
 
 if __name__ == "__main__":
-    main():
+    main()
